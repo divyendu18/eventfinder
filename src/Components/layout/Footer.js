a8b808cedfa532992c27e5c4bbce4ff2a8a4b214
divyendu18 <d4divyendu18@gmail.com>
2020-06-13 15:27:20 +0530

    first commit

--- src/Components/layout/Footer.js
+++ src/Components/layout/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <div className="footer">
-            <p>Made with <i className="fa fa-heart" aria-hidden="true"></i> By Abhi Kumar.</p>
+            <p>Made with <i className="fa fa-heart" aria-hidden="true"></i> By Divyendu Jha.</p>
             <p>copyright &copy; {new Date().getFullYear()}</p>
         </div>
     )
